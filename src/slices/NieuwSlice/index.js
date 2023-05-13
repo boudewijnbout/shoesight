4099dff807e5bbf1d20d18ee3bca2533dff0de81
Shauri Maigua <90189750+SCNMC@users.noreply.github.com>
2023-05-13 16:00:13 +0200

    Made nieuw section

--- src/slices/NieuwSlice/index.js
+++ src/slices/NieuwSlice/index.js
@@ -1,8 +1,7 @@
 import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
-import Image from 'next/image';
-import styles from "../../styles/slices/nieuwSlice.module.css"
-
+import Image from "next/image";
+import styles from "../../styles/slices/nieuwSlice.module.css";
 
 /**
  * @typedef {import("@prismicio/client").Content.NieuwSliceSlice} NieuwSliceSlice
@@ -11,13 +10,29 @@ import styles from "../../styles/slices/nieuwSlice.module.css"
  */
 
 const NieuwSlice = ({ slice }) => {
-  return (
-    
-    <section className={styles.nieuw}>
-            <h2><PrismicRichText field={slice.primary.title} /></h2>
-    </section>
-  );
+	const articles = slice.items.map((obj) => obj.articlelink.data);
+
+	return (
+		<section className={styles.nieuw}>
+			<PrismicRichText field={slice.primary.title} />
+
+			<div>
+				{articles.map((article) => {
+					return (
+						<>
+							<article>
+								<picture>
+									<PrismicNextImage field={article.featuredimage} />
+								</picture>
+								<PrismicRichText field={article.title} />
+								<PrismicRichText field={article.shortdescription} />
+							</article>
+						</>
+					);
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default NieuwSlice;
-
