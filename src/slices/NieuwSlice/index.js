94a0145b52c2745c4dfb39f539542c3f4a5da8a7
Boudewijn Bout <boudewijnbout@MBPvanBoudewijn.fritz.box>
2023-05-16 15:36:42 +0200

    feat: Made improvents to nieuwSlice

--- src/slices/NieuwSlice/index.js
+++ src/slices/NieuwSlice/index.js
@@ -1,28 +1,32 @@
 import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
-import Image from "next/image";
 import styles from "../../styles/slices/nieuwSlice.module.css";
+import Label from "@/components/Label";
 
 /**
  * @typedef {import("@prismicio/client").Content.NieuwSliceSlice} NieuwSliceSlice
  * @typedef {import("@prismicio/react").SliceComponentProps<NieuwSliceSlice>} NieuwSliceProps
  * @param {NieuwSliceProps}
  */
-
 const NieuwSlice = ({ slice }) => {
 	const articles = slice.items.map((obj) => obj.articlelink.data);
 
 	return (
 		<section className={styles.nieuw}>
+
+			{/* Section title */}
 			<PrismicRichText field={slice.primary.title} />
 
 			<div>
 				{articles.map((article) => {
 					return (
 						<>
+
+							{/* Nieuw article */}
 							<article>
 								<picture>
 									<PrismicNextImage field={article.featuredimage} />
+									<Label title={article.label[0].text} />
 								</picture>
 								<PrismicRichText field={article.title} />
 								<PrismicRichText field={article.shortdescription} />
