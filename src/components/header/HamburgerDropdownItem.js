a0330d9947c309705c75e39a584b8fc7baaf193e
Boudewijn Bout <boudewijnbout@MacBook-Pro-van-Boudewijn.local>
2023-05-17 14:45:01 +0200

    feat: Added header components

---
+++ src/components/header/HamburgerDropdownItem.js
@@ -0,0 +1,14 @@
+import classes from "./HamburgerMenu.module.css";
+import Link from "next/link";
+
+const HamburgerDropdownItem = (props) => {
+  return (
+    <li>
+      <Link className={classes.linkTitle} href={props.link}>
+        {props.linkTitle}
+      </Link>
+    </li>
+  );
+};
+
+export default HamburgerDropdownItem;
