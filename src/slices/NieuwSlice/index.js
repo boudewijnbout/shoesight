520a3a520e9e69589528f8d0796f9f50613739fe
boudewijnbout <boudewijn_bout@outlook.com>
2023-05-29 17:46:28 +0200

    feat: Replaced flexbox with grid + added hover animations

--- src/slices/NieuwSlice/index.js
+++ src/slices/NieuwSlice/index.js
@@ -1,6 +1,10 @@
 import { PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
+
+// Styles
 import styles from "../../styles/slices/nieuwSlice.module.css";
+
+// Components
 import Label from "@/components/Label";
 
 /**
@@ -13,16 +17,12 @@ const NieuwSlice = ({ slice }) => {
 
 	return (
 		<section className={styles.nieuw}>
-
-			{/* Section title */}
 			<PrismicRichText field={slice.primary.title} />
 
 			<div>
 				{articles.map((article) => {
 					return (
 						<>
-
-							{/* Nieuw article */}
 							<article>
 								<picture>
 									<PrismicNextImage priority sizes="(max-width: 30rem) 100vw, (max-width: 75rem) 33vw, 33vw" field={article.featuredimage} />
