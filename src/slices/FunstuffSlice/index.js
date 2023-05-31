cf509a46b89e376f81c0277f231bf80548efe318
Finnvb <finnvb@gmail.com>
2023-05-31 16:10:23 +0200

    New grid layout made

--- src/slices/FunstuffSlice/index.js
+++ src/slices/FunstuffSlice/index.js
@@ -28,9 +28,10 @@ const FunstuffSlice = ({ slice }) => {
                   />
                   <Label title={article.label[0].text} />
                 </picture>
-
-                <PrismicRichText field={article.title} />
-                <PrismicRichText field={article.shortdescription} />
+                <div>
+                  <PrismicRichText field={article.title} />
+                  <PrismicRichText field={article.shortdescription} />
+                </div>
               </article>
             </>
           );
