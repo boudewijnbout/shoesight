7e9c554b7baa31000ac19f74b0429abf27ab805d
boudewijnbout <boudewijn_bout@outlook.com>
2023-06-02 19:48:19 +0200

    feat: Added ISR revalidate time

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import { SliceZone } from "@prismicio/react";
 import { createClient } from "../../prismicio";
 import { components } from "../slices";
-// import Header from ".././components/header/Header";
+
 const Page = ({ page }) => {
   return <SliceZone slices={page.data.slices} components={components} />;
 };
@@ -20,5 +20,7 @@ export async function getStaticProps({ previewData }) {
     props: {
       page,
     },
+
+    revalidate: 60,
   };
 }
